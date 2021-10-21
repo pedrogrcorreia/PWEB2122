184e00db0ee2affafdc42d7b440349adaf179a2f
Pedro Correia <pedrogrcorreia@gmail.com>
2021-10-21 16:02:44 +0100

    minor fix quadrado

--- Ficha 1/Ex3/Quadrado.cs
+++ Ficha 1/Ex3/Quadrado.cs
@@ -6,21 +6,10 @@ namespace f1ex3
     {
         public Quadrado(double lado):base(lado, lado){}
 
-
-        public override double Perimetro()
-        {
-            return base.Perimetro();
-        }
-
         public override string ToString()
         {
             return $"\nQuadrado: Lado={Comprimento:F2}"
             + $" Area={Area():F2} Perimetro={Perimetro():F2}\n";
-        }
-
-        public override double Area()
-        {
-            return base.Area();
         }
     }
 }
