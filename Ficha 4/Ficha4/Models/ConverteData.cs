f0c1132b89b36c1562d686153f7201e343d71c01
Pedro Correia <pedrogrcorreia@gmail.com>
2022-01-14 13:03:59 +0000

    change folder name

---
+++ Ficha 4/Ficha4/Models/ConverteData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ficha4.Models
+{
+    public partial class ConverteData
+    {
+        int Id { get; set; }
+        [Display(Name = "Moeda Inicial")]
+        public int UnidadeInicial { get; set; }
+
+        [Display(Name = "Moeda Final")]
+        public int UnidadeFinal { get; set; }
+
+        [Display(Name = "Valor Inicial")]
+        public double ValorInicial { get; set; }
+
+        [Display(Name = "Valor Final")]
+        public double ValorFinal { get; set; }
+
+        [Display(Name = "Taxa de Câmbio")]
+        public double TaxaCambio { get; set; }
+    }
+}
